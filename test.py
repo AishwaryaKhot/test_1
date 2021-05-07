93deb6a98c4049f41d81ac87a176df05ce89ca8c
Aishwarya Khot <aish.khot.alex@gmail.com>
2021-05-07 20:49:27 +0200

    dkjsfjksd

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import pygame 
 # this is the mew chkjdfsnajögndskönfsdk
 #git does not recognize my chnages
+there are many things we need to learn
+# can we see these chanhes ? 
+# this si not sdkjgkjdfshkdslksd kjdshfkjsdnhflsd
